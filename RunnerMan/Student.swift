7c2e033ea523ca67f3ef67da63ed7aa42c68891d
RyanAu624 <ryanaulh@gmail.com>
2022-01-10 21:22:10 +0800

    Import Class and basic setup

---
+++ RunnerMan/Student.swift
@@ -0,0 +1,16 @@
+//
+//  Student.swift
+//  RunnerMan
+//
+//  Created by Long Hei Au on 10/1/2022.
+//
+
+import Foundation
+
+struct Student {
+    var studentID : String
+    var studentName : String
+    var studentEmail : String
+    var studentWeight : Double
+    var studentHeight : Double
+}
---
+++ RunnerMan/Teacher.swift
@@ -0,0 +1,13 @@
+//
+//  Teacher.swift
+//  RunnerMan
+//
+//  Created by Long Hei Au on 10/1/2022.
+//
+
+import Foundation
+
+struct Teacher {
+    var teacherName : String
+    var teacherEmail : String
+}
