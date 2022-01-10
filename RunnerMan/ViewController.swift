ebc02ce746b3374c8e2286cff2b25d75d79d534a
RyanAu624 <ryanaulh@gmail.com>
2022-01-10 18:28:45 +0800

    Initial Commit

---
+++ RunnerMan/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RunnerMan
+//
+//  Created by Long Hei Au on 10/1/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
