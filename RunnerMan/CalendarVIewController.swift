6bd16d1c71c69c2b1d412e409bb431d47637fc3f
dgfd <tonykong679@gmail.com>
2022-03-14 17:21:22 +0800

    developing

---
+++ RunnerMan/CalendarVIewController.swift
@@ -0,0 +1,16 @@
+//
+//  CalendarVIewController.swift
+//  RunnerMan
+//
+//  Created by TonyKong on 14/3/2022.
+//
+
+import UIKit
+import CalendarKit
+
+class CalendarVIewController: DayViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "calendar"
+    }
+}
