7f92c719ba416c6a9b26b2e20987e9239dc6256d
dgfd <tonykong679@gmail.com>
2022-03-30 11:45:27 +0800

    can show the event

--- RunnerMan/CalendarVIewController.swift
+++ RunnerMan/CalendarVIewController.swift
@@ -7,10 +7,33 @@
 
 import UIKit
 import CalendarKit
+import EventKit
 
-class CalendarVIewController: DayViewController {
+class CalendarVIewController: DayViewController{
+    private let eventStore = EKEventStore()
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "calendar"
+        requestAccessToCalendar()
+    }
+    
+    func requestAccessToCalendar(){
+        eventStore.requestAccess(to: .event) {success, error in
+            
+        }
+    }
+    override func eventsForDate(_ date: Date) -> [EventDescriptor] {
+        let startDate = date
+        var oneDay = DateComponents()
+        oneDay.day = 1
+        
+        let endDate = calendar.date(byAdding: oneDay, to: startDate)!
+        
+        let predicate = eventStore.predicateForEvents(withStart: startDate, end: endDate, calendars: nil)
+        
+        let eventKitEvents = eventStore.events(matching: predicate)
+        
+        let calendarKitEvents = eventKitEvents.map(EKWrapper.init)
+        return calendarKitEvents
     }
 }
