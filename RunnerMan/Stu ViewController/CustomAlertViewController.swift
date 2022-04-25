00df9d3d4cfea58b506b7c8357c646540f59df5f
RyanAu624 <ryanaulh@gmail.com>
2022-04-25 14:21:59 +0800

    customAlert update

--- RunnerMan/Stu ViewController/CustomAlertViewController.swift
+++ RunnerMan/Stu ViewController/CustomAlertViewController.swift
@@ -16,10 +16,15 @@ class CustomAlertViewController: UIViewController {
     var training = [Training]()
     var list = [Training]()
     
-    private var userInput: String
+    let uID = Auth.auth().currentUser?.uid
+    
+    private var inputTrainingID: String = ""
     
     @IBOutlet weak var popUPView: UIView!
     @IBOutlet weak var activityListBtn: UIButton!
+    
+    @IBOutlet weak var descriptionTF: UITextField!
+    @IBOutlet weak var uploadVideoBtn: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,7 +36,9 @@ class CustomAlertViewController: UIViewController {
         popUPView.layer.shadowRadius = 5
 
         let optionsClosure = { (action: UIAction) in
-          print(action.title)
+            self.findTrainID(title: action.title, findCompetion: { (result) in
+                self.inputTrainingID = result
+            })
         }
         
         getTrainingList(loadCompletion : {
@@ -45,6 +52,28 @@ class CustomAlertViewController: UIViewController {
         activityListBtn.showsMenuAsPrimaryAction = true
         activityListBtn.changesSelectionAsPrimaryAction = true
     }
+    
+    //******************
+    func findTrainID(title: String, findCompetion: (_ result : String) -> Void) {
+        
+        var postID = ""
+        
+        let ref = db.collection("Training").whereField("Training Method", isEqualTo: title)
+
+        ref.getDocuments {(snapshot, err) in
+            if err == nil {
+                if let snapshot = snapshot {
+                    for document in snapshot.documents {
+                        postID = document.data()["Postid"] as! String
+                        print("$$$\(postID)$$$")
+                    }
+                }
+            }
+        }
+        print("***\(postID)***")
+        findCompetion("\(postID)")
+    }
+    //******************
     
     func getTrainingList(loadCompletion : @escaping ()->Void) {
         db.collection("Training").getDocuments() {(snapshot, err) in
@@ -79,6 +108,34 @@ class CustomAlertViewController: UIViewController {
     }
     
     @IBAction func uploadBtn(_ sender: Any) {
+//        let activity = self.userInput
+        let des = descriptionTF.text
+        
+//        let ref = db.collection("Training").whereField("Training Method", isEqualTo: activity)
+//        let ref = db.collection("Training").document(list.trainingID)
+//        let secRef = ref.firestore.collection("participant").document(uID!)
+//
+//        ref.updateData([""])
+        
+        
+//        let stuNumber = studentNumberTF.text
+//        let stuName = studentNameTF.text
+//        let stuPhoneNum = studentPhoneNumTF.text
+//        let stuClass = studentClassTF.text
+//        let stuAge = studentAgeTF.text
+//        let stuWeight = studentWeightTF.text
+//        let stuHeight = studentHeightTF.text
+//
+//        let ref = db.collection("student").document(uID!)
+//
+//        ref.updateData(["studentId":stuNumber as Any,
+//                        "studentName":stuName as Any,
+//                        "studentContactNumber":stuPhoneNum as Any,
+//                        "studentClass":stuClass as Any,
+//                        "studentAge":stuAge as Any,
+//                        "studentWeight":stuWeight as Any,
+//                        "studentHeight":stuHeight as Any])
+//    }
     }
     
     @IBAction func cancelBtn(_ sender: Any) {
